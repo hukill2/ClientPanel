7082880797a92746ce63f889391dc477969a2cb0
Forest Hukill <forest@foresthukill.com>
2019-01-28 12:31:17 -0800

    settings

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Login from "./components/auth/Login";
 import AppNavbar from "./components/layout/AppNavbar";
 import Dashboard from "./components/layout/Dashboard";
 import ClientDetails from "./components/clients/ClientDetails";
+import Settings from "./components/settings/Settings";
 import "./App.css";
 
 class App extends Component {
@@ -45,6 +46,11 @@ class App extends Component {
                   exact
                   path="/login"
                   component={UserIsNotAuthenticated(Login)}
+                />
+                <Route
+                  exact
+                  path="/settings"
+                  component={UserIsAuthenticated(Settings)}
                 />
               </Switch>
             </div>
---
+++ src/components/settings/Settings.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { Link } from "react-router";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import {
+  setAllowRegistration,
+  setDisableBalanceOnAdd,
+  setDisableBalanceOnEdit
+} from "../../actions/settingsActions";
+
+class Settings extends Component {
+  render() {
+    const {
+      disableBalanceOnAdd,
+      disableBalanceOnEdit,
+      allowRegistration
+    } = this.props.settings;
+
+    return (
+      <div>
+        <div className="row">
+          <div className="col-md-6">
+            <Link to="/" className="btn btn-link">
+              <i className="fas fa-arrow-circle-left" /> Back To Dashboard
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Settings.propTypes = {
+  settings: PropTypes.object.isRequired,
+  setDisableBalanceOnAdd: PropTypes.func.isRequired,
+  setDisableBalanceOnEdit: PropTypes.func.isRequired,
+  setAllowRegistration: PropTypes.func.isRequired
+};
+
+export default connect(
+  (state, props) => ({
+    auth: state.firebase.auth,
+    settings: state.settings
+  }),
+  { setAllowRegistration, setDisableBalanceOnAdd, setDisableBalanceOnEdit }
+)(Settings);
