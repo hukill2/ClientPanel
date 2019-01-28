2a7edee67a901cf4a78bc82d579910d682c5ca8a
Forest Hukill <forest@foresthukill.com>
2019-01-27 19:00:44 -0800

    edit client details functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import AddClient from "./components/clients/AddClient";
+import EditClient from "./components/clients/EditClient";
 import AppNavbar from "./components/layout/AppNavbar";
 import Dashboard from "./components/layout/Dashboard";
 import ClientDetails from "./components/clients/ClientDetails";
@@ -19,6 +20,7 @@ class App extends Component {
               <Switch>
                 <Route exact path="/" component={Dashboard} />
                 <Route exact path="/client/add" component={AddClient} />
+                <Route exact path="/client/edit/:id" component={EditClient} />
                 <Route exact path="/client/:id" component={ClientDetails} />
               </Switch>
             </div>
